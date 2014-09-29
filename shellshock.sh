ccd8aeba36f26db182291a838388a247464855b6
SaltwaterC <saltwaterc@gmail.com>
2014-09-29 14:50:24 +0300

    Added tests for the new vulns related to shellshock.

--- shellshock.sh
+++ shellshock.sh
@@ -18,18 +18,46 @@ function cve_2014_7169
 	rm -f echo
 }
 
+function cve_2014_7186
+{
+	echo "${bold}Testing $1 for CVE-2014-7186${normal}"
+	{bash -c "true $(printf '<<EOF %.0s' {1..79})"} > /dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+		echo "VULNERABLE"
+	fi
+}
+
+function cve_2014_7187
+{
+	echo "${bold}Testing $1 for CVE-2014-7187${normal}"
+	$1 -c "`for i in {1..200}; do echo -n "for x$i in; do :;"; done; for i in {1..200}; do echo -n "done;";done`" 2>/dev/null
+	if [ $? -ne 0 ]
+	then
+		echo "VULNERABLE"
+	else
+		echo "Test not reliable without bash being built with -fsanitize=address"
+	fi
+}
+
+function shellshock_test
+{
+	cve_2014_6271 "$1"
+	cve_2014_7169 "$1"
+	cve_2014_7186 "$1"
+	cve_2014_7187 "$1"
+}
+
 for path in ${PATH//:/ }
 do
 	if [ -f "$path/sh" ]
 	then
-		cve_2014_6271 "$path/sh"
-		cve_2014_7169 "$path/sh"
+		shellshock_test "$path/sh"
 		echo
 	fi
 	if [ -f "$path/bash" ]
 	then
-		cve_2014_6271 "$path/bash"
-		cve_2014_7169 "$path/bash"
+		shellshock_test "$path/bash"
 		echo
 	fi
 done
